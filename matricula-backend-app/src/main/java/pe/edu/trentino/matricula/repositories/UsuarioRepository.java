c068fbd6f300d4c21ab0f7f5aea95e85d1b48050
Leodanperez <leodanperez.1998@gmail.com>
2024-10-28 22:32:27 -0500

    Fix subiendo cambios

--- matricula-backend-app/src/main/java/pe/edu/trentino/matricula/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.trentino.matricula.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.trentino.matricula.models.Usuario;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
